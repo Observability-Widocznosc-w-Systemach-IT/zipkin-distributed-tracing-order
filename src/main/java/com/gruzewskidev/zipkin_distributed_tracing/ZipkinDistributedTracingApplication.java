b88a06e21e557458297829ecc4d8328d5df8d32f
Mateusz Grużewski <mateusz.gruzewski@zut.edu.pl>
2025-02-07 11:22:16 +0100

    init

---
+++ src/main/java/com/gruzewskidev/zipkin_distributed_tracing/ZipkinDistributedTracingApplication.java
@@ -0,0 +1,13 @@
+package com.gruzewskidev.zipkin_distributed_tracing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipkinDistributedTracingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinDistributedTracingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gruzewskidev/zipkin_distributed_tracing/ZipkinDistributedTracingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gruzewskidev.zipkin_distributed_tracing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipkinDistributedTracingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
